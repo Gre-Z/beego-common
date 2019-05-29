5ad5b0ffd3427f5923d7d37536b38067c228ea74
Gre-z <golang.zgl@gmail.com>
2019-05-29 16:46:45 +0800

    修改validate错误提示

--- validate/setting.go
+++ validate/setting.go
@@ -2,8 +2,8 @@ package validate
 
 import "github.com/astaxie/beego"
 
-var nonExistent = "【%s】必须存在   /   注释:%s"
-var formatErr = "【%s】内容不符合要求   /   注释:%s"
+var nonExistent = "字段【%s】:必须存在   /   注释:%s"
+var formatErr = "字段【%s】:值不符合   /   注释:%s"
 var isDebug = true
 
 func SetNonExistent(format string) {
